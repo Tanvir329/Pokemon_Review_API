de9fa1c4def4f3c0a408c22b2f3784005e24a51f
Tanvir.Attar <tanvir.attar@medimaze.in>
2025-04-25 11:08:36 +0530

    Add Foreign keys with onetomany and manytoone

--- src/main/java/com/pokemonreview/api/models/Pokemon.java
+++ src/main/java/com/pokemonreview/api/models/Pokemon.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -16,6 +20,8 @@ public class Pokemon {
     private String name;
     private String type;
 
+    @OneToMany(mappedBy = "pokemon", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<Review>();
 }
 
 
